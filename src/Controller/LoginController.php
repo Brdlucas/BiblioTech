acd6890a0c2b752d2dc65f658eda059cb7203b0c
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-03-14 17:57:52 +0100

    Mise à jour du contrôleur de connexion : ajout de la méthode POST pour la connexion et gestion de la requête

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\SearchType;
+use Google\Service\ServiceConsumerManagement\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route(path: '/connexion', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    #[Route(path: '/connexion', name: 'app_login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('app_homepage');
@@ -21,6 +24,7 @@ class LoginController extends AbstractController
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
 
         return $this->render('login/login.html.twig', [
             'last_username' => $lastUsername,
