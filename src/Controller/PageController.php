6f93e7ebfd51f4ae313abe0382624c00574f69f8
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-03-17 16:21:33 +0100

    Ajout de la gestion de l'historique des emprunts dans le contrôleur de page

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Borrowing;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class PageController extends AbstractController
 {
@@ -13,6 +15,20 @@ final class PageController extends AbstractController
     {
         return $this->render('page/index.html.twig', [
             'controller_name' => 'PageController',
+        ]);
+    }
+
+    #[Route('/borrowing/history', name: 'app_borrowing_history')]
+    public function borrowing(EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $borrowings = $entityManager->getRepository(Borrowing::class)->findBy(['userbook' => $user->getId()]);
+
+
+        return $this->render('page/borrowing.html.twig', [
+            'controller_name' => 'PageController',
+            'borrowings' => $borrowings,
         ]);
     }
 }
