c95a73273d1910566aed21865ea9c08a6ecfbcb1
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-03-19 00:22:43 +0100

    Mise à jour du contrôleur UserCrud pour désactiver l'édition des champs 'email', 'prénom', 'nom', 'vérifié', 'date de création' et 'date de modification', tout en rendant le champ 'rôles' modifiable.

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,14 +19,39 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('email')
+                ->setLabel('Email')
+                ->setFormTypeOption('disabled', true), // Affiché mais non modifiable
+
+            TextField::new('firstname')
+                ->setLabel('Prénom')
+                ->setFormTypeOption('disabled', true), // Affiché mais non modifiable
+
+            TextField::new('lastname')
+                ->setLabel('Nom')
+                ->setFormTypeOption('disabled', true)
+                ->onlyOnIndex(), // Affiché seulement dans la liste
+
+            ArrayField::new('roles')
+                ->setLabel('Rôles'), // Seul champ modifiable
+
+            BooleanField::new('isVerified')
+                ->setLabel('Vérifié')
+                ->setFormTypeOption('disabled', true)
+                ->hideOnForm(),
+
+            DateField::new('created_at')
+                ->setLabel('Date de création')
+                ->hideOnForm() // Affiché mais pas dans les formulaires
+                ->setSortable(true),
+
+            DateField::new('updated_at')
+                ->setLabel('Date de modification')
+                ->hideOnForm() // Affiché mais pas dans les formulaires
+                ->setSortable(true),
         ];
     }
-    */
 }
